2aab18c51f8cdb0baad3dfadfc02cf893cacacd2
dobin <dobin@broken.ch>
2024-08-05 09:21:44 +0200

    refactor: serialize

--- RedEdr/procinfo.h
+++ RedEdr/procinfo.h
@@ -27,6 +27,28 @@ public:
         wprintf(L"         working_dir: %ls\n", working_dir.c_str());
     }
 
+    wchar_t* serialize() const {
+        // Calculate the total size needed for the serialized string
+        size_t totalSize = 0;
+        totalSize += std::to_wstring(id).length() + 1; // For id
+        totalSize += std::to_wstring(parent_pid).length() + 1; // For parent_pid
+        totalSize += 1; // For observe
+        totalSize += image_path.length() + 1;
+        totalSize += commandline.length() + 1;
+        totalSize += working_dir.length() + 1;
+
+        // Allocate memory for the serialized string
+        wchar_t* serializedString = (wchar_t*)malloc((totalSize + 1) * sizeof(wchar_t));
+        if (!serializedString) {
+            return nullptr; // Allocation failed
+        }
+
+        // Copy the data into the serialized string
+        swprintf(serializedString, totalSize + 1, L"%u;%u;%d;%ls;%ls;%ls", id, parent_pid, observe, image_path.c_str(), commandline.c_str(), working_dir.c_str());
+
+        return serializedString;
+    }
+
     BOOL doObserve() {
         return observe;
     }
