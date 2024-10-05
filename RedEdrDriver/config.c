42b51f608868ec6ef8a5061d6033c5ac9850f905
dobin <dobin@broken.ch>
2024-10-05 21:22:15 +0200

    fix: re-enable kernelmode threads

--- RedEdrDriver/config.c
+++ RedEdrDriver/config.c
@@ -9,9 +9,9 @@ Config g_config;
 
 void init_config() {
 	g_config.init_processnotify = 1;
-	g_config.init_threadnotify = 0; // no need for now
+	g_config.init_threadnotify = 1;
 	g_config.init_imagenotify = 1;
-	g_config.init_obnotify = 0; // too much data
+	g_config.init_obnotify = 0; // too much data, no parser
 
 	g_config.enable_kapc_injection = 0;
 	g_config.enable_logging = 0;
