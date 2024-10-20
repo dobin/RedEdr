ccb3877a68952ba8fdb6f6fc9b94f090a116d212
dobin <dobin@broken.ch>
2024-10-20 14:53:37 +0200

    feature: initial better web ui

--- RedEdr/webserver.cpp
+++ RedEdr/webserver.cpp
@@ -11,11 +11,36 @@ HANDLE webserver_thread;
 httplib::Server svr;
 
 
+std::string read_file(const std::string& path) {
+    std::ifstream file(path);
+    if (!file.is_open()) {
+        std::cerr << "Could not open file: " << path << std::endl;
+        return "";
+    }
+    std::stringstream buffer;
+    buffer << file.rdbuf(); // Read the file into the stringstream
+    return buffer.str();
+}
+
+
+
 DWORD WINAPI WebserverThread(LPVOID param) {
     LOG_A(LOG_INFO, "!WEB: Start Webserver thread");
+    
     svr.Get("/", [](const httplib::Request&, httplib::Response& res) {
+        std::string indexhtml = read_file("index.html");
+        res.set_content(indexhtml, "text/html");
+    });
+
+    svr.Get("/api/events", [](const httplib::Request&, httplib::Response& res) {
         res.set_content(g_EventProducer.GetAllAsJson(), "application/json; charset=UTF-8");
-        });
+    });
+
+    svr.Get("/api/stats", [](const httplib::Request&, httplib::Response& res) {
+        std::string stats = "{ \"bla\": 1, \"gna\": 2 }";
+        res.set_content(stats, "application/json; charset=UTF-8");
+    });
+
     LOG_A(LOG_INFO, "WEB: Web Server listening on http://localhost:8080");
     svr.listen("localhost", 8080);
     LOG_A(LOG_INFO, "!WEB: Exit Webserver thread");
