eaf01692114b8a00837a10e5ba44d0f74bfaa219
Dobin Rutishauser <dobin.rutishauser@gmail.com>
2024-12-19 20:36:20 +0100

    fix: Unittest includes

--- UnitTests/UnitTestProcessInfo.cpp
+++ UnitTests/UnitTestProcessInfo.cpp
@@ -10,9 +10,10 @@
 #include "config.h"
 #include "utils.h"
 
-#include "processcache.h"
+#include "process_resolver.h"
+#include "mem_static.h"
 #include "process_query.h"
-#include "meminfo.h"
+#include "process_resolver.h"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
@@ -25,37 +26,37 @@ namespace UnitTests
 
         TEST_METHOD(MemInfoBasics)
         {
-            TargetInfo targetInfo = TargetInfo();
+            MemStatic memStatic = MemStatic();
 			MemoryRegion* region = new MemoryRegion("test", 0x1000, 0x1000, "rwx");
-			targetInfo.AddMemoryRegion(0x1000, region);
-			Assert::IsTrue(targetInfo.ExistMemoryRegion(0x1000));
-			Assert::IsFalse(targetInfo.ExistMemoryRegion(0x2000));
+			memStatic.AddMemoryRegion(0x1000, region);
+			Assert::IsTrue(memStatic.ExistMemoryRegion(0x1000));
+			Assert::IsFalse(memStatic.ExistMemoryRegion(0x2000));
 
-			MemoryRegion* region2 = targetInfo.GetMemoryRegion(0x1000);
+			MemoryRegion* region2 = memStatic.GetMemoryRegion(0x1000);
 			Assert::IsNotNull(region2);
 			Assert::AreEqual(region2->name, region->name);
 			Assert::AreEqual(region2->addr, region->addr);
 			Assert::AreEqual(region2->size, region->size);
 			Assert::AreEqual(region2->protection, region->protection);
 
-			targetInfo.RemoveMemoryRegion(0x1000, 0x1000);
-			Assert::IsFalse(targetInfo.ExistMemoryRegion(0x1000));
+			memStatic.RemoveMemoryRegion(0x1000, 0x1000);
+			Assert::IsFalse(memStatic.ExistMemoryRegion(0x1000));
         }
 
         TEST_METHOD(MemInfoMultiple)
         {
-			TargetInfo targetInfo = TargetInfo();
+			MemStatic memStatic = MemStatic();
 			MemoryRegion* region = new MemoryRegion("test", 0x1000, 0x1000, "rwx");
-			targetInfo.AddMemoryRegion(0x1000, region);
-			Assert::IsTrue(targetInfo.ExistMemoryRegion(0x1000));
-			Assert::IsFalse(targetInfo.ExistMemoryRegion(0x2000));
+			memStatic.AddMemoryRegion(0x1000, region);
+			Assert::IsTrue(memStatic.ExistMemoryRegion(0x1000));
+			Assert::IsFalse(memStatic.ExistMemoryRegion(0x2000));
 
 			MemoryRegion* region2 = new MemoryRegion("test2", 0x2000, 0x1000, "rwx");
-			targetInfo.AddMemoryRegion(0x2000, region2);
-			Assert::IsTrue(targetInfo.ExistMemoryRegion(0x2000));
-			Assert::IsFalse(targetInfo.ExistMemoryRegion(0x3000));
+			memStatic.AddMemoryRegion(0x2000, region2);
+			Assert::IsTrue(memStatic.ExistMemoryRegion(0x2000));
+			Assert::IsFalse(memStatic.ExistMemoryRegion(0x3000));
                 
-			MemoryRegion* region3 = targetInfo.GetMemoryRegion(0x2000);
+			MemoryRegion* region3 = memStatic.GetMemoryRegion(0x2000);
 			Assert::IsNotNull(region3);
 			Assert::AreEqual(region3->name, region2->name);
 			Assert::AreEqual(region3->addr, region2->addr);
@@ -63,10 +64,10 @@ namespace UnitTests
 
         TEST_METHOD(MemInfoUsage)
         {
-            TargetInfo targetInfo = TargetInfo();
+            MemStatic memStatic = MemStatic();
             MemoryRegion* region = new MemoryRegion("test", 0x1000, 0x1000, "rwx");
-            targetInfo.AddMemoryRegion(0x1000, region);
-            std::string resolved = targetInfo.ResolveStr(0x1000);
+            memStatic.AddMemoryRegion(0x1000, region);
+            std::string resolved = memStatic.ResolveStr(0x1000);
 			Assert::AreEqual(resolved.c_str(), "test");
         }
     };
@@ -82,17 +83,17 @@ namespace UnitTests
 
             // PID 1 doesnt exist usually
             // But entry should still be created
-            p = g_ProcessCache.getObject(1);
+            p = g_ProcessResolver.getObject(1);
             Assert::IsNotNull(p);
             Assert::IsFalse(p->observe);
             
-            Assert::IsTrue(g_ProcessCache.containsObject(1));
+            Assert::IsTrue(g_ProcessResolver.containsObject(1));
 
             g_config.targetExeName = L"explorer.exe";
             std::wstring processName = L"explorer.exe";
             DWORD pid = FindProcessIdByName(processName);
             Assert::IsTrue(pid > 0);
-            p = g_ProcessCache.getObject(pid);
+            p = g_ProcessResolver.getObject(pid);
             Assert::IsNotNull(p);
             Assert::IsTrue(p->observe);
             Assert::IsTrue(contains_case_insensitive(p->image_path, processName));
@@ -106,7 +107,7 @@ namespace UnitTests
             Process* p = MakeProcess(pid, processName.c_str());
             
             Assert::IsTrue(pid > 0);
-            p = g_ProcessCache.getObject(pid);
+            p = g_ProcessResolver.getObject(pid);
             Assert::IsNotNull(p);
             Assert::IsTrue(p->observe);
             Assert::IsTrue(contains_case_insensitive(p->image_path, processName));
