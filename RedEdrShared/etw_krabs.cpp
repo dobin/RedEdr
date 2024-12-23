93754f3604b8196ecc9967e11fead043fa93b58c
dobin <dobin@broken.ch>
2024-12-23 10:44:03 +0100

    fix: callstack

--- RedEdrShared/etw_krabs.cpp
+++ RedEdrShared/etw_krabs.cpp
@@ -104,12 +104,13 @@ std::wstring KrabsEtwEventToJsonStr(const EVENT_RECORD& record, krabs::schema sc
     }
 
     // Callstack
+    j["stack_trace"] = nlohmann::json::array();
     auto stack_trace = schema.stack_trace();
     for (auto& return_address : stack_trace)
     {
         // Only add non-kernelspace addresses
         if (return_address < 0xFFFF080000000000) {
-            j["stack_trace"] += { "addr", return_address };
+            j["stack_trace"].push_back({ { "addr", return_address} });
         }
     }
 
