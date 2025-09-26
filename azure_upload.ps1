70606d481d5ecaceb989943eb19091c583bb24f6
Dobin <dobin.rutishauser@gmail.com>
2025-09-26 14:55:02 +0100

    fix: nicer output

--- azure_upload.ps1
+++ azure_upload.ps1
@@ -3,17 +3,23 @@ $config = Get-Content -Raw -Path ".\azure_config.json" | ConvertFrom-Json
 
 # cleanup
 $path = "C:\rededr\data"
+$source = "C:\rededr\*"
+$destination = "C:\rededr\rededr.zip"
+
+Write-Output "cleanup..."
 if (Test-Path $path) {
     Get-ChildItem -Path $path -File | Remove-Item -Force -ErrorAction SilentlyContinue
 }
-Get-ChildItem "c:\rededr\rededr.zip" | Remove-Item -Force -ErrorAction SilentlyContinue
+if (Test-Path $destination) {
+  Get-ChildItem $destination | Remove-Item -Force -ErrorAction SilentlyContinue
+}
 
 # make a zip
-$source = "C:\rededr\*"
-$destination = "C:\rededr\rededr.zip"
+Write-Output "zip..."
 Compress-Archive -Path $source -DestinationPath $destination -Force
 
 # Upload zip as blob
+Write-Output "upload..."
 az storage blob upload `
   --account-name $($config.StorageAccount) `
   --container-name $($config.ContainerName) `
@@ -23,4 +29,5 @@ az storage blob upload `
   --overwrite
 
 # delete zip
+Write-Output "cleanup..."
 Get-ChildItem "c:\rededr\rededr.zip" | Remove-Item -Force
