2c51fd1e5b300435d81b6202e208ed62e0282842
Dobin Rutishauser <dobin.rutishauser@gmail.com>
2026-06-22 18:37:20 +0200

    feature: compile help for vsc

--- sign_file.ps1
+++ sign_file.ps1
@@ -1,6 +1,7 @@
 # Change this:
 $password = "password"
 
-
-# signtool.exe sign /fd SHA256 /a /v /ph /f $args[0] /p $password $args[1]
-signtool.exe sign /fd SHA256 /a /ph /f $args[0] /p $password $args[1]
+# Completely suppress all output
+$ErrorActionPreference = 'SilentlyContinue'
+$null = & signtool.exe sign /fd SHA256 /a /ph /f $args[0] /p $password $args[1] 2>&1
+exit 0
